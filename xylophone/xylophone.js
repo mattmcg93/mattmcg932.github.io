4676dcb1f295c6ecb82d765eceb74a8bb552cb61
mattmcg93 <mm891@exeter.ac.uk>
2024-12-20 12:09:58 +0000

    Update xylophone.js

--- xylophone/xylophone.js
+++ xylophone/xylophone.js
@@ -1,37 +1,40 @@
 var wordNumber = 0;
 var soundsPath = "sounds/";
-var extension = ".mp3"
+var extension = ".mp3";
+
+// Corresponding text to display
+var words = ["Leo", "You", "Cant", "Catch", "Me"];
 
 var manifest = [
-    [ // You
+    [ // Leo
         "1_1",
         "1_2",
         "1_3",
         "1_4",
         "1_5",
     ],
-    [ // Are
+    [ // You
         "2_1",
         "2_2",
         "2_3",
         "2_4",
         "2_5",
     ],
-    [ // A
+    [ // Cant
         "3_1",
         "3_2",
         "3_3",
         "3_4",
         "3_5",
     ],
-    [ // Fucking
+    [ // Catch
         "4_1",
         "4_2",
         "4_3",
         "4_4",
         "4_5",
     ],
-    [ // Cunt
+    [ // Me
         "5_1",
         "5_2",
         "5_3",
@@ -39,10 +42,14 @@ var manifest = [
         "5_5",
     ]];
 
-
 function playSound(target) {
+    // Play the sound
     new Audio(soundsPath + manifest[wordNumber][target.id] + extension).play();
 
+    // Update the text above the xylophone
+    document.getElementById("displayText").textContent = words[wordNumber];
+
+    // Cycle to the next word
     if (wordNumber < 4) {
         wordNumber++;
     } else {
